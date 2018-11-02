006115f33db759710f3cc07b737b027976d4ce8b
spinarunsal <spnrunsal@gmail.com>
2018-11-02 02:08:56 -0300

    Inherited swipe and delete functionality from its super class

--- Todoey/Controllers/SwipeTableViewController.swift
+++ Todoey/Controllers/SwipeTableViewController.swift
@@ -14,7 +14,7 @@ class SwipeTableViewController: UITableViewController, SwipeTableViewCellDelegat
     var cell: UITableViewCell?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.rowHeight = 80.0
     }
     
     //TableView Data Source Methods
--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 import RealmSwift
 
-
-class TodoListViewController: UITableViewController{
+class TodoListViewController: SwipeTableViewController {
+    
     var todoItems: Results<Item>?
     let realm = try! Realm()
     
@@ -34,7 +34,7 @@ class TodoListViewController: UITableViewController{
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         if let item = todoItems?[indexPath.row] {
             cell.textLabel?.text = item.title
@@ -110,7 +110,20 @@ class TodoListViewController: UITableViewController{
         todoItems = selectedCategory?.items.sorted(byKeyPath: "title", ascending: true)
         tableView.reloadData()
     }
+    
+    override func updateModel(at indexPath: IndexPath) {
+        if let item = todoItems?[indexPath.row] {
+                do {
+                    try self.realm.write {
+                        self.realm.delete(item)
+                    }
+                } catch {
+                    print("Error Deleting Item \(error)")
+                }
+        }
+    }
 }
+
 
 //MARK: - Search bar methods
 
